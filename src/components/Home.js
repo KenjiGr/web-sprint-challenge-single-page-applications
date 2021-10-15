d956346591c1cabd4f23ac0709a8a782186c629d
Kenji Greene <kenji@Kenjis-MacBook-Air.local>
2021-10-15 15:30:59 -0400

    created home component

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Home() {
+
+    return (
+        <div className="homePage">
+           <nav>
+             <h1>Lambda Pizza</h1>
+             <img alt='pizza' src="https://images.unsplash.com/photo-1595854341625-f33ee10dbf94?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1770&q=80" />          
+           </nav>
+       </div>
+    );
+}
+
+       export default Home;
